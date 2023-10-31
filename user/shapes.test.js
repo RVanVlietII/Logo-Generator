db80388acab24c11bf4b0050db7020a37441670d
RVanVlietII <rcvanvlietii@gmail.com>
2023-10-31 11:18:49 -0400

    fixed shapes.test.js file to include what shapes to actually check for when rendered

--- user/shapes.test.js
+++ user/shapes.test.js
@@ -5,16 +5,15 @@ describe("Triangle test", () => {
     it("should test for a triangle with a gray background", () => {
         const shape = new Triangle();
         shape.useColor("gray");
-        expect();
-    }
-    )
+        expect(shape.render()).toBe('<polygon points="150, 18 244, 183 56, 182" fill="gray" />');
+    })
 });
 
 describe("Square test", () => {
     it("should test for a square with a red background", () => {
         const shape = new Square();
-        shape.useColor("rede");
-        expect();
+        shape.useColor("red");
+        expect(shape.render()).toBe('<rect x="73" y="40" width="160" height="160" fill="red" />');
     })
 });
 
@@ -22,7 +21,7 @@ describe("Circle test", () => {
     it("should test for a circle with a black background", () => {
         const shape = new Circle();
         shape.useColor("black");
-        expect();
+        expect(shape.render()).toBe('<circle cx="150" cy="115" r="80" fill="black" />');
     })
 });
 
